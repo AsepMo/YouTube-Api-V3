50d4eb84871e24f4681012172f8cde0b29ea1b26
AsepMo <asepmo.story@gmail.com>
2021-05-22 06:17:33 +0700

    update application

---
+++ Application/app/src/main/java/com/youtube/example/fragments/BerandaFragment.java
@@ -0,0 +1,21 @@
+package com.youtube.example.fragments;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.View;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.youtube.example.R;
+
+public class BerandaFragment extends Fragment
+{
+
+    public static String TAG = BerandaFragment.class.getSimpleName();
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_beranda, container, false);
+    }
+
+}
